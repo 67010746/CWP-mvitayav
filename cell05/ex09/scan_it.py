54cc0bf2c50db7761ed97468a6290f98fb8babc8
mvitayav <67010746@kmitl.ac.th>
2026-09-25 12:38:03 +0700

    Create scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,11 @@
+import sys
+import re
+
+if len(sys.argv) == 3:
+    matches = re.findall(sys.argv[1], sys.argv[2])
+    if matches:
+        print(len(matches))
+    else:
+        print("none")
+else:
+    print("none")
